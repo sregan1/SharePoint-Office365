c5ef52cf41a81e2f0a14468ae6bbb60ab54b704f
Sean Regan <6962880+sregan1@users.noreply.github.com>
2018-02-07 18:29:11 -0500

    Adding Shared Documents

---
+++ Shared Documents/SiteAssets/Scripts/shared_documents_search.js
@@ -0,0 +1,111 @@
+﻿<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+
+	// Get a querystring parameter from a given url and key
+	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+
+	// Update a querystring parameter given a url, key and value
+	function updateQueryStringParameter(url, key, value) {
+	  var re = new RegExp("([?#&])" + key + "=.*?(&|$)", "i");
+	  var separator = url.indexOf('?') !== -1 ? "&" : "?";
+	  var refiners = url.indexOf('#') !== -1 ? url.substring(url.indexOf('#')) : "";
+		  
+	  if (url.match(re)) {
+	    return url.replace(re, '$1' + key + "=" + value + '$2' + refiners);
+	  }
+	  else {
+	    return url + separator + key + "=" + value + refiners;
+	  }
+	}
+	
+	// Submit the search based on clicking on the button or pressing enter
+	function submitSearch()
+	{
+		var searchTerm = $("#txtSearch").val();
+		
+		var url = window.location.href;
+				
+		url = updateQueryStringParameter(url, "searchTerm", searchTerm);
+				
+		window.location.href = url;	
+	}
+
+
+	$(document).ready(function ()
+	{	   
+		// Submit on clicking the button
+    	$("#btnSubmit").on("click", function() {   				
+				submitSearch();		
+ 		});
+ 		
+ 		// Submit on pressing enter
+ 		$("#txtSearch").keypress(function(e) {
+		    if(e.which == 13) {
+		    	submitSearch();
+			}
+		});
+		
+		// If there is a search term, show it in the box
+		var searchTerm = getParameterByName("searchTerm");
+		if (searchTerm != "*")
+		{
+			document.getElementById("txtSearch").value	= searchTerm;
+		}
+	}); 
+	
+	
+	// If the page has no parameters, redirect with a wildcard search term (otherwise the SCWP won't bring back results)	
+    var searchTerm = getParameterByName("searchTerm");     
+    if (searchTerm == null)
+    {
+    	window.location.replace(window.location.href + "?searchTerm=*")
+    }	
+	
+</script>
+
+<style>
+	.button, .button:hover {
+	    background-color: #008CBA; 
+	    border: none;
+	    color: white;
+	    padding: 5px 10px;
+	    text-align: center;
+	    text-decoration: none;
+	    display: inline-block;
+	    font-size: 16px;
+	    border-radius: 8px;
+	    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 3px 10px 0 rgba(0,0,0,0.19);
+	}
+	tr, td {
+		padding: 10px 10px 10px 10px;
+	}
+	}
+</style>
+
+<div id="new-site-message">
+</div>
+<div id="new-site-form">
+	<table>
+		<tr>
+			<td>
+				Search for a document here:
+			</td>
+			<td>
+				<input id="txtSearch"></textarea>
+			</td>
+			<td>
+				<div id="btnSubmit" class="button">Submit</div>
+			</td>
+		</tr>
+	</table>
+</div>
+
---
+++ Shared Documents/SiteAssets/Scripts/shared_documents_styles.js
@@ -0,0 +1,13 @@
+﻿// Add a style string via js
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
+
+// Set styles for the Search Results web part
+addStyleString('.cbs-Item {padding-bottom: 2px;} .cbs-Item h3 {padding-top: 10px;} .ms-promlink-headerNav {float: left;} .cbs-Line2 {padding-left: 10px;} .folder-path {color: #BBB !important; padding-left: 10px;}');
+
+// Set styles for Site Refinement web part
+addStyleString('.teamLandingTable { display: inline-table; width: 33%; } .teamLandingTitle { font-family: flama; font-weight: normal; font-style: normal; font-size:14pt; max-width:33%; color:#414141 !important; } .teamLandingDescription{ word-wrap:break-word; padding-bottom:30px; padding-right:20px; }');
+
